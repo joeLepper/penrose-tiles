d2627ca88063597502e0fbb1a3470c555b9b6722
Joe Lepper <josephlepper@gmail.com>
2021-10-29 12:37:43 -0700

    favicons and titles

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,48 +6,4 @@ export { vectorBetween } from './vector-between'
 export { calculateVectorEnd } from './calculate-vector-end'
 export { calculateKiteApex } from './calculate-kite-apex'
 export { calculateDartApex } from './calculate-dart-apex'
-
-export let fills = [
-  '#111',
-  '#333',
-  '#666',
-  '#aaa',
-  '#ccc',
-  '#111',
-  '#f09',
-  '#f90',
-  '#9f0',
-  '#90f',
-]
-
-export let strokes = [
-  '#999',
-  '#eee',
-  '#666',
-  '#aaa',
-  '#ccc',
-  '#90f',
-  '#f09',
-  '#f90',
-  '#9f0',
-]
-
-const shuffle = (arr) => {
-  let currentIndex = arr.length
-  let randomIndex
-  while (currentIndex != 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex)
-    currentIndex--
-    [arr[currentIndex], arr[randomIndex]] = [
-      arr[randomIndex], arr[currentIndex]]
-  }
-  return arr;
-}
-
-export const shuffleFills = () => {
-  fills = shuffle(fills)
-}
-
-export const shuffleStrokes = () => {
-  strokes = shuffle(strokes)
-}
+export { fills, strokes, shuffleFills, shuffleStrokes } from './colors'
